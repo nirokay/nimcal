b178f25d8c5899479c8e96d38c1036ca35280ad1
nirokay <nirokay-public@protonmail.com>
2025-01-02 22:07:24 +0100

    types a bit reworked

--- src/typedefs.nim
+++ src/typedefs.nim
@@ -1,5 +1,27 @@
+import std/[
+    strutils,
+    tables
+]
+export
+    tables
+
+
+# -----------------------------------------------------------------------------
+# Declarations:
+# -----------------------------------------------------------------------------
 
 type
+    CalendarPropertyType* = enum
+        propertyString, propertyObject
+    CalendarPropertyTable* = OrderedTable[string, CalendarProperty] ## "Property Parameter" -> "Property Parameter Value" (described in https://datatracker.ietf.org/doc/html/rfc5545.html#section-3.2)
+    CalendarProperty* = object
+        case propertyType*: CalendarPropertyType:
+        of propertyString:
+            text*: string
+        of propertyObject:
+            data*: CalendarPropertyTable
+
+
     CalendarItemIdentifier* = enum
         VEVENT    ## https://datatracker.ietf.org/doc/html/rfc5545.html#section-3.6.1
         VTODO     ## https://datatracker.ietf.org/doc/html/rfc5545.html#section-3.6.2
@@ -8,23 +30,74 @@ type
         VTIMEZONE ## https://datatracker.ietf.org/doc/html/rfc5545.html#section-3.6.5
         VALARM    ## https://datatracker.ietf.org/doc/html/rfc5545.html#section-3.6.6
 
-    CalendarItem* = object of RootObj
+    CalendarItem* = object
         identifier: CalendarItemIdentifier ## is set via its respective `newItem()` proc
-
-    CalendarEvent* = object of CalendarItem
-    CalendarToDo* = object of CalendarItem
-    CalendarJournal* = object of CalendarItem
-    CalendarFreeBusy* = object of CalendarItem
-    CalendarTimezone* = object of CalendarItem
-    CalendarAlarm* = object of CalendarItem
+        properties*: CalendarPropertyTable
 
     Calendar* = object
         version*: string = "2.0"
-        children*: seq[CalendarItem]
+        identifier*: string = "VCALENDAR"
+        children*: seq[string]
+        properties*: CalendarPropertyTable
 
-proc newCalendarEvent*(): CalendarEvent = CalendarEvent(identifier: VEVENT)
-proc newCalendarToDo*(): CalendarToDo = CalendarToDo(identifier: VTODO)
-proc newCalendarJournal*(): CalendarJournal = CalendarJournal(identifier: VJOURNAL)
-proc newCalendarFreeBusy*(): CalendarFreeBusy = CalendarFreeBusy(identifier: VFREEBUSY)
-proc newCalendarTimezone*(): CalendarTimezone = CalendarTimezone(identifier: VTIMEZONE)
-proc newCalendarAlarm*(): CalendarAlarm = CalendarAlarm(identifier: VTIMEZONE)
+
+proc newCalendarEvent*(): CalendarItem = CalendarItem(identifier: VEVENT)
+proc newCalendarToDo*(): CalendarItem = CalendarItem(identifier: VTODO)
+proc newCalendarJournal*(): CalendarItem = CalendarItem(identifier: VJOURNAL)
+proc newCalendarFreeBusy*(): CalendarItem = CalendarItem(identifier: VFREEBUSY)
+proc newCalendarTimezone*(): CalendarItem = CalendarItem(identifier: VTIMEZONE)
+proc newCalendarAlarm*(): CalendarItem = CalendarItem(identifier: VTIMEZONE)
+
+proc newCalendarPropertyString*(text: string): CalendarProperty =
+    result = CalendarProperty(
+        propertyType: propertyString,
+        text: text
+    )
+proc newCalendarPropertyObject*(data: CalendarPropertyTable): CalendarProperty =
+    result = CalendarProperty(
+        propertyType: propertyObject,
+        data: data
+    )
+
+
+# -----------------------------------------------------------------------------
+# String/Dollar procs:
+# -----------------------------------------------------------------------------
+
+proc propertyTextString(property: CalendarProperty): string =
+    result = ":"
+    # TODO: implementation
+proc propertyTextObject(property: CalendarProperty): string =
+    result = ";"
+    # TODO: implementation
+proc propertyText(property: CalendarProperty): string =
+    ## Processes text of properties, quotes them if needed
+    result = case property.propertyType:
+        of propertyString: property.propertyTextString()
+        of propertyObject: property.propertyTextObject()
+
+proc `$`*(item: CalendarItem): string =
+    result = "BEGIN:" & $item.identifier
+    for key, value in item.properties:
+        result &= "\n" & key & propertyText(value)
+    result &= "\nEND:" & $item.identifier
+
+proc `$`*(calendar: Calendar): string =
+    # Header:
+    result = @[
+        "BEGIN:" & calendar.identifier,
+        "VERSION:" & calendar.version
+    ].join("\n")
+
+    # Children:
+    for child in calendar.children:
+        result &= "\n" & $child
+
+    # Closure:
+    result &= "\n" & "END:" & calendar.identifier
+
+
+# -----------------------------------------------------------------------------
+# :
+# -----------------------------------------------------------------------------
+
---
+++ tests/test_compile.nim
@@ -0,0 +1,5 @@
+import std/[unittest]
+import nimcal
+
+test "Compilation":
+    check true
